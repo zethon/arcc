53123bc453457b6ea0fbd6d4b2664f506548b0a7
Addy <aclaure@gmail.com>
2017-12-13 14:42:15 -0500

    Issue #1: finsihed implementing stub console program for Windows;

---
+++ src/ConsoleWindows.cpp
@@ -0,0 +1,58 @@
+// Another Reddit Console Client
+// Copyright (c) 2017, Adalid Claure <aclaure@gmail.com>
+
+#include "Console.h"
+
+#include <conio.h>
+
+namespace arcc
+{
+namespace console
+{
+
+ConsoleHandler::ConsoleHandler() = default;
+ConsoleHandler::~ConsoleHandler() = default;
+
+void ConsoleHandler::run()
+{
+    bool done { false };
+
+    while (!done)
+    {
+        const int c = _getch();
+
+        switch (c)
+        {
+            default:
+                onChar(c);
+            break;
+
+            case '\r':
+                done = onEnter();
+            break;
+
+            case '\b':
+                onBackSpace();
+            break;
+
+            case 0:
+            case 0xe0:
+            {
+                const int escchar = _getch();
+                switch (escchar)
+                {
+                    default:
+                    break;
+
+                    case 83:
+                        onDelete();
+                    break;
+                }
+            }
+            break;
+        }
+    }
+}
+
+} // namespace console
+} // namespace arcc
