d99029ed3c2de61d80491678bac90e482a01dfc9
Addy <aclaure@gmail.com>
2019-11-30 09:16:07 -0500

    moving on

--- appveyor/install_coverage.cmd
+++ appveyor/install_coverage.cmd
@@ -1,14 +1,5 @@
 echo "Installing OpenCppCoverage"
-
-REM "choco install opencppcoverage" suddenly broke on AppVeyor, so let's 
-REM manually grab the installer and install install
-
-REM choco install opencppcoverage
-wget 'https://github.com/OpenCppCoverage/OpenCppCoverage/releases/download/release-0.9.8.0/OpenCppCoverageSetup-x64-0.9.6.1.exe' -OutFile coverage_setup.exe
-coverage_setup.exe /VERYSILENT
+choco install opencppcoverage
 set path=C:\Program Files\OpenCppCoverage;%PATH%
-
-echo "Installing CodeCov"
 choco install codecov
 set path=C:\ProgramData\chocolatey\lib\codecov\tools;%PATH%
-
